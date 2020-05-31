f17249b7d2f8f7269195bc615cfcbf141ec648a5
aseem wangoo <aseemwangoo@gmail.com>
2020-05-31 09:03:28 +0800

    [C/Dev] Include logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:exp_with_leaflet/app_level/routes/routes.dart';
 import 'package:exp_with_leaflet/app_level/theme/theme.dart';
+import 'package:exp_with_leaflet/locator.dart';
 
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'package:logging/logging.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // INIT DEP INJECTION..
+  setupLocator();
 
   // SETUP ORIENTATION
   if (!kIsWeb) {
     await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   }
+
+  // LOGGING
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    print('👻👻👻 ${record.level.name}: 🎯🎯 ${record.message}');
+  });
 
   runApp(MyApp());
 }
