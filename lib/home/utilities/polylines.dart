f744e57730ca3040ab509c34560e514e765989b0
aseem wangoo <aseemwangoo@gmail.com>
2020-06-01 22:49:25 +0800

    [C/Dev] Display polylines

--- lib/home/utilities/polylines.dart
+++ lib/home/utilities/polylines.dart
@@ -1,6 +1,5 @@
 import 'package:exp_with_leaflet/app_level/models/api_response.dart';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
@@ -12,18 +11,7 @@ class MapUtils {
     var _positions = <LatLng>[];
 
     for (var position in points) {
-      var _latitude = 0.0;
-      var _longitude = 0.0;
-
-      if (position.length == 3) {
-        _latitude = position.first.clamp(-90.0, 90.0);
-        _longitude = position[1].clamp(-180.0, 180.0);
-      } else {
-        _latitude = position.first.clamp(-90.0, 90.0);
-        _longitude = position.last.clamp(-180.0, 180.0);
-      }
-
-      _positions.add(LatLng(_latitude, _longitude));
+      _positions.add(validator(position));
     }
 
     return _positions;
@@ -38,7 +26,7 @@ class MapUtils {
       _polylines.add(
         Polyline(
           points: polylines(_points),
-          strokeWidth: 4.0,
+          strokeWidth: 1.0,
           color: Colors.red,
         ),
       );
