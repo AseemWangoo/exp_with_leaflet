6a325319639295530ab266bcec9ede978ac79a4b
aseem wangoo <aseemwangoo@gmail.com>
2020-06-06 10:08:16 +0800

    [C/Dev] Current location

---
+++ lib/app_level/services/locations/location_service.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/rendering.dart';
+import 'package:location/location.dart';
+
+import 'package:logging/logging.dart';
+import 'package:latlong/latlong.dart';
+
+class LocationService {
+  LocationService() : _location = Location() {
+    _location.onLocationChanged.listen(_onChanged);
+  }
+
+  // --------------------------------------------- EXPOSED ---------------------------------------------
+
+  LatLng get currentLatLng => _currentLatLng;
+
+  // --------------------------------------------- INTERNALS ---------------------------------------------
+
+  final Location _location;
+  LatLng _currentLatLng;
+  static final _locationServiceLogger = Logger('LocationService');
+
+  void _onChanged(LocationData current) {
+    try {
+      //
+      debugPrint('CURRE LOCATION ${current.toString()}');
+      _currentLatLng.latitude = current.latitude;
+      _currentLatLng.longitude = current.longitude;
+    } catch (exc) {
+      _locationServiceLogger.severe(exc.toString());
+    }
+    return null;
+  }
+}
